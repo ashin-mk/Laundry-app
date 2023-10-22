f914df5cb7318219b15114734690aabe2d3a3b35
ashin krishna mk <ashindeedu315@gmail.com>
2023-10-22 12:52:05 +0530

    ------deploy

--- Laundry-Server/index.js
+++ Laundry-Server/index.js
@@ -12,7 +12,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
